3e9105fd7657c750c846f68d8eb044185d8efd19
lazuardi100 <lazuardi.akhmad@gmail.com>
2021-05-31 09:56:53 +0700

    pasing args

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def index():
     data["val_umur"] = umur
     return json.dumps(data)
 
+@app.route('/input/<int:id>', methods=['GET'])
+def input_id(id):
+    parameter = str(id)
+    return parameter
+
+@app.route('/input/post/', methods=['POST'])
+def input_post():
+    args1 = request.args.get("nama")
+    return args1
+
 if __name__ == '__main__':
     # app.run()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
